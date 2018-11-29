a300ef30be3303de931773eeb3f20a6c4664ac66
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2018-11-29 13:43:04 +0100

    rad tokom tutta

--- src/ba/unsa/etf/rpr/tutorijal6/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal6/Main.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception
+    {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Formular");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
